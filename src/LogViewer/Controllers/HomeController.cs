cf9361cb407a56b5f887b09510c8f1a2e410888e
Konrad Grohs <konrad.grohs@alertsense.com>
2015-10-08 15:04:05 -0600

    GG-24 GG-26 - Initial commit with .gitignore in place. Copied from Gauge repo. Basic MVC pattern up and running, Working on Hosting and Database configuration on Lumber-Jack server.

---
+++ src/LogViewer/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using LogViewer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lumberjack.LogViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ILog log = LogManager.GetLogger(typeof(HomeController));
+            log.Debug("LoadingHomeController.");
+
+            //TODO: Get logs from DB
+            IEnumerable<Log4NetLog> logs = new List<Log4NetLog> {
+                new Log4NetLog{Message = "Message1"},
+                new Log4NetLog{Message = "Message2"},
+                new Log4NetLog{Message = "Message3"},
+            };
+            return View(logs);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
