ec9e62a0c49da36e53dc5f553dba4a9ed4d2cadc
Konrad Grohs <konrad.grohs@alertsense.com>
2015-10-09 11:17:20 -0600

    Changed the default page and added table to index view.

--- src/LogViewer/App_Start/RouteConfig.cs
+++ src/LogViewer/App_Start/RouteConfig.cs
@@ -16,7 +16,13 @@ namespace LogViewer
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "LogViewer", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "LogViewer",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "LogViewer", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ src/LogViewer/Controllers/LogViewerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using LogViewer.Models;
+using Lumberjack.LogViewer.Controllers;
+
+namespace LogViewer.Controllers
+{
+    public class LogViewerController : Controller
+    {
+        //
+        // GET: /LogViewer/
+
+        public ActionResult Index()
+        {
+            ILog log = LogManager.GetLogger(typeof(HomeController));
+            log.Debug("LoadingHomeController.");
+
+            
+            IEnumerable<Log4NetLog> logs = new List<Log4NetLog> {
+                new Log4NetLog{Message = "Message1"},
+                new Log4NetLog{Message = "Message2"},
+                new Log4NetLog{Message = "Message3"},
+            };
+            return View(logs);
+        }
+
+    }
+}
